1d46ee29a7aa2f5aac72e0904f27bd734fe0c5c3
Guilherme Pereira <guiihpr@gmail.com>
2016-04-30 14:43:56 +0200

    First commit

---
+++ src/br/com/unip/aps/init/Init.java
@@ -0,0 +1,14 @@
+package br.com.unip.aps.init;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Init {
+	public static void main(String[] args) {
+		Random random = new Random();
+		
+		
+		
+	}
+}
